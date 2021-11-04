09532ceef50a7d87fc5e3dd041bb356c9a238975
Qioqvi <56963437+Qioqvi@users.noreply.github.com>
2021-11-05 01:40:27 +1100

    temp settings for movement

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
+    private Vector3 nextPosition = new Vector3(-10, 5, 0);
+    private Vector3 initialPosition;
+    private float testDuration = 1f;
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        float percentageComplete = time / testDuration;
+        transform.position = Vector3.Lerp(initialPosition, nextPosition, percentageComplete);
     }
 }
